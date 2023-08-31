ad3d3cad9bd876a2cc55294a529c1fef9de8a2d3
hangie5 <142239368+hangie5@users.noreply.github.com>
2023-09-01 01:03:20 +0200

    Update ft_strlen.c

--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -10,22 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include <string.h> //include the string.h header for the standard strlen function
+#include <stdio.h> //incolude the stdio.h header for input and output functions
 
-int	ft_strlen(char *str)
+int	ft_strlen(char *str) //define ft_strlen to calculate the length of a string
+//function definition for a 'ft_strlen'. it takes 'str' as an argument, which is a pointer to the beginning of a string
 {
-	int	len;
+	int	len; //declare integer variable named 'len' to store the length of the string
 
-	len = 0;
-	while (str[len])
+	len = 0; //initialize 'len' variable to 0, so the length of the string is currently 0
+	while (str[len]) //
 		len++;
 	return (len);
 }
-/*
+
 int main(void)
 {
 	printf("%lu\n", strlen(""));
 	printf("%d\n", ft_strlen(""));
 }
-*/
